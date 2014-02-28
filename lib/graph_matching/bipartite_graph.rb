38f77a662292ac7d69f88554e95324a5e4b27371
Jared Beck <jared@jaredbeck.com>
2014-02-27 23:13:06 -0500

    Extracts method `Matching#augment`

--- lib/graph_matching/bipartite_graph.rb
+++ lib/graph_matching/bipartite_graph.rb
@@ -115,20 +115,7 @@ module GraphMatching
           puts "Unable to find an augmenting path.  We're done!"
           break
         else
-          raise "invalid path: must have length of at least two" unless augmenting_path.length >= 2
-          augmenting_path_edges = []
-          0.upto(augmenting_path.length - 2).each do |j|
-            augmenting_path_edges << [augmenting_path[j], augmenting_path[j + 1]]
-          end
-          raise "invalid augmenting path: must have odd length" unless augmenting_path_edges.length.odd?
-          puts "augmenting the matching"
-          augmenting_path_edges.each_with_index do |edge, ix|
-            if ix.even?
-              m.add(edge)
-            else
-              m.delete_if { |e| array_match?(e, edge) }
-            end
-          end
+          m.augment(augmenting_path)
         end
 
         stage += 1
@@ -160,10 +147,6 @@ module GraphMatching
     def add_to_set(set, vertex:, fail_if_in:)
       raise NotBipartiteError if fail_if_in.include?(vertex)
       set.add(vertex)
-    end
-
-    def array_match?(a, b)
-      a.sort == b.sort
     end
 
     def assert_disjoint(u, v)
--- lib/graph_matching/matching.rb
+++ lib/graph_matching/matching.rb
@@ -11,5 +11,28 @@ module GraphMatching
       end
     end
 
+    def augment(augmenting_path)
+      raise "invalid path: must have length of at least two" unless augmenting_path.length >= 2
+      augmenting_path_edges = []
+      0.upto(augmenting_path.length - 2).each do |j|
+        augmenting_path_edges << [augmenting_path[j], augmenting_path[j + 1]]
+      end
+      raise "invalid augmenting path: must have odd length" unless augmenting_path_edges.length.odd?
+      puts "augmenting the matching"
+      augmenting_path_edges.each_with_index do |edge, ix|
+        if ix.even?
+          add(edge)
+        else
+          delete_if { |e| array_match?(e, edge) }
+        end
+      end
+    end
+
+  private
+
+    def array_match?(a, b)
+      a.sort == b.sort
+    end
+
   end
 end
