e689ec8fe7839045af655cabea56f251120b5498
Jared Beck <jared@jaredbeck.com>
2014-09-23 00:30:47 -0400

    Import description of Gabow's algorithm

--- lib/graph_matching/graph.rb
+++ lib/graph_matching/graph.rb
@@ -56,11 +56,113 @@ module GraphMatching
       (u.nil? || m.empty?) ? m : mcm_stage(m, u)
     end
 
-    # `mcm_stage` - Given a matching `m` and an unmatched
-    # vertex `ri`, returns an augmented matching.
-    def mcm_stage(m, ri)
+    # `e` constructs a maximum matching on a graph.  It starts a
+    # search for an augmenting path to each unmatched vertex u.
+    # It scans edges of the graph, deciding to assign new labels
+    # or to augment the matching.
+    def e(m)
       log('mcm_stage: matching: ' + m.inspect)
       fail 'TODO'
+
+      # E0. [Imtiahze.] Read the graph into adjacency lists,
+      # numbering the vertices 1 to V and the edges V -4- 1 to
+      # V + 2W. Create a dummy vertex 0 For 0 < i < V, set
+      # LABEL(u) ~- --1, MATE(i) ~- 0 (all vertices are nonouter
+      # and unmatched) Set u ~- 0
+      #
+      # El. [Find unmatched vertex ] Set u ~-~u + 1. If u > V,
+      # halt; MATE contains a maximum matching Otherwise, if vertex
+      # u is matched, repeat step E1 Otherwise (u is unmatched, so
+      # assign a start label and begin a new search) set LABEL(u)
+      # ~-- FIRST(u) ~- O.
+      #
+      # E2 [Choose an edge ] Choose an edge xy, where x ]s an outer
+      # vertex. (An edge vw may be chosen twice in a search--once
+      # with x = v, and once with x = w.) If no such edge exists,
+      # go to E7. (Edges xy can be chosen m an arbitrary order. A
+      # posmble chome method m "breadth-first": an outer vertex
+      # x = xt is chosen, and edges x~y are chosen m succeeding
+      # executions of E2, when all such edges have been chosen, the
+      # vertex x~ that was labeled immediately after x~ is chosen,
+      # and the process is re-peated for x = x~. This breadth-first
+      # method requires that Algorithm E maintain a list of outer
+      # vertices, xL, x~ ••• .)
+      #
+      # E3. [Augment the matching.l If y is unmatched and y ¢ u, set
+      # MATE(y) ~--x, call R(x, y): then go to E7 (R completes the
+      # augment along path (y)*P(x))
+      #
+      # E4. [Assign edge labels.] If y is outer, call L, then go to
+      # E2 (L assigns edge label n(xy) to nonouter vertmes in P(x)
+      # and P(y))
+      #
+      # E5. [Assign a vertex label.] Set v ~-~MATE(y). If v m
+      # nonouter, set LABEL(v) ~ x, FIRST(v) ~- y, and go to E2
+      #
+      # E6. [Get next edge.] Go to E2 (y is nonouter and MATE(y) is
+      # outer, so edge xy adds nothing).
+      #
+      # E7. [Stop the search] Set LABEL(O) *-- --1. For all outer
+      # vertices % set LABEL(q) e- LABEL (MATE(z)) ~-- --1 Then go
+      # to E1 (now all vertmes are nonouter for the next search).
+      #
+    end
+
+    # L assigns the edge label n(xy) to nonouter vertices Edge xy
+    # joins outer vertices x, y. L sets join to the first nonouter
+    # vertex m both P(z) and P(y). Then it labels all nonouter
+    # vertices preceding join in P(x) or P(y)
+    def l
+      # L0. [Initialize.] Set r ~ FIRST(x), s ~-- FIRST(y).
+      # If r = s, return (no vertices can be labeled).
+      # Otherwise flag r and s. (Steps L1-L2 find 3oin by advancing
+      # alternately along paths P(x) and P(y). Flags are assigned
+      # to nonouter vertices r in these paths. This is done by
+      # setting LABEL(r) to a negative edge number, LABEL(r)
+      # +- -n(xy). This way, each invocation of L uses a distinct
+      # flag value.)
+      #
+      # L1. [Switch paths ] If s ~ 0, interchange r and s, r ~-~s
+      # (r is a flagged nonouter vertex, alternately in P(x) and P(y)).
+      #
+      # L2. [Next nonouter vertex.] Set r +-- FIRST(LABEL(MATE(r)))
+      # (r ]s set to the next nonouter vertex in P(x) or P(y)). If
+      # r is not flagged, flag r and go to L1 Otherwise set3oin ~--
+      # r and go to L3.
+      #
+      # L3. [Label vertices in P(x), P(y).] (All nonouter vertmes
+      # between x and 3o~n, or y and jozn, will be assigned edge
+      # labels. See Figure 4(a).) Set v +- FIRST(x) and do L4. Then
+      # set v +- FIRST(y) and do L4. Then go to L5.
+      #
+      # L4 [Label v]Ifv~ 3oin,setLABEL(v) +-n(xy), FIRST(v) ~-)oln,
+      # v~--FIRST(LABEL(MATE(v))) and repeat step L4
+      # Otherwise continue as specified in L3.
+      #
+      # L5 [Update FIRST.] For each outer vertex i, if FIRST(I) is
+      # outer, set FIRST(z) +- 3o~n. (Join is now the first nonouter
+      # vertex in P(i) )
+      #
+      # L6. [Done ] Return
+      #
+    end
+
+    # R (v, w) rematches edges m the augmenting path. Vertex v is
+    # outer. Part of path (w)*P(v) is in the augmenting path. It
+    # gets rematehed by R(v, w) (Although R sets MATE(v) +- w, it
+    # does not set MATE(w) ~- v. This is done in step E3 or another
+    # call to R.) R is a recursive routine.
+    def r
+      # R1. [Match v to w ] Set t ~-- MATE(v), MATE(v) ~- w. If
+      # MATE(t) ~ v, return (the path m com- pletely rematehed)
+      #
+      # R2. [Rematch a path.] If v has a vertex label, set
+      # MATE(t) ~-- LABEL(v), call R(LABEL(v), t) recursivcly, and
+      # then return.
+      #
+      # R3. [Rematch two paths.] (Vertex v has an edge label ) Set
+      # x, y to vertices so LABEL(v) = n(xy), call R(x, y)
+      # recurslvely, call R(y, x) recurslvely, and then return.
     end
 
     def print(base_filename)
