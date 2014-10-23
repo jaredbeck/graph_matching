703f4e53986fb3fd5ed54af43e89422ddd23dfc8
Jared Beck <jared@jaredbeck.com>
2014-10-23 00:08:11 -0400

    Add `Explainable#timestamp` for quick and dirty profiling

--- lib/graph_matching/explainable.rb
+++ lib/graph_matching/explainable.rb
@@ -4,4 +4,12 @@ module Explainable
     puts msg if ENV['EXPLAIN']
   end
 
+  # `timestamp` is only used in development, for very simple profiling.
+  def timestamp(id)
+    t = Time.now.to_f
+    delta = $gm_ts.nil? ? 0 : t - $gm_ts
+    puts '%20.4f %20.4f %s' % [t, delta, id]
+    $gm_ts = t
+  end
+
 end
