7114cbffa0e4df4e46587ec1ac1501114dcc5f9a
Jared Beck <jared@jaredbeck.com>
2015-01-18 11:47:39 -0500

    Begin benchmarking MWMBipartite

---
+++ benchmark/mwm_bipartite/complete_bigraphs/benchmark.rb
@@ -0,0 +1,41 @@
+# No shebang here.  Usage:
+# ruby -I lib benchmark/mwm_bipartite/complete_bigraphs/benchmark.rb
+
+require 'benchmark'
+require 'graph_matching'
+
+MIN_SIZE = 300
+MAX_SIZE = 303
+
+$stdout.sync = true
+
+# Returns a bipartite graph that is:
+#
+# 1. Complete - Each vertex in U has an edge to each vertex in V
+# 2. Weighted - Each edge has a different, consecutive integer weight
+# 3. "Balanced" - In an even bigraph, U and V have the same number of
+#    vertexes.  In an odd bigraph, U has one more than V.
+#    0 <= abs(U - V) <= 1
+#
+def complete_weighted_bigraph(n)
+  g = GraphMatching::Graph::WeightedBigraph.new
+  max_u = (n.to_f / 2).ceil
+  min_v = max_u + 1
+  weight = 1
+  1.upto(max_u) do |i|
+    min_v.upto(n) do |j|
+      g.add_edge(i, j)
+      g.set_w([i, j], weight)
+      weight += 1
+    end
+  end
+  g
+end
+
+MIN_SIZE.upto(MAX_SIZE) do |v|
+  print "%5d\t" % [v]
+  g = complete_weighted_bigraph(v)
+  GC.disable
+  puts Benchmark.realtime { g.maximum_weighted_matching }
+  GC.enable
+end
---
+++ benchmark/mwm_bipartite/complete_bigraphs/plot.gnuplot
@@ -0,0 +1,21 @@
+data_dir = "~/git/jaredbeck/graph_matching/benchmark/mwm_bipartite/complete_bigraphs"
+
+set title "MWM in Complete Bigraph is O(n ** (3/4) m log N)"
+set key left box
+set term png size 800, 500
+set output data_dir."/plot.png"
+
+set linetype 1 pointtype 7
+set linetype 2 linewidth 3
+
+set xlabel 'Number of Vertexes (n)' tc lt 1
+set ytics autofreq tc lt 1
+set ylabel 'Time (s)' tc lt 1
+set y2tics autofreq tc lt 2
+set y2label 'n ** (3/4) m log N' tc lt 2
+
+plot \
+data_dir."/time.data" every 1:1:1::449 \
+using 1:2 title "Time (s)" lt 1 axes x1y1, \
+data_dir."/nmN.data" every 1:1:1::449 \
+using 1:8 title "n ** (3/4) m log N" with lines lt 2 axes x1y2
