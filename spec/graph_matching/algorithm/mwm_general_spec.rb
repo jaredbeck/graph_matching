ecd04ec41c874d31b8187665881d67d8f30cd5cd
Jared Beck <jared@jaredbeck.com>
2015-01-18 23:21:12 -0500

    Begin implementing Gabow 1985

--- spec/graph_matching/algorithm/mwm_general_spec.rb
+++ spec/graph_matching/algorithm/mwm_general_spec.rb
@@ -6,10 +6,40 @@ RSpec.describe GraphMatching::Algorithm::MWMGeneral do
   describe '#match' do
     context 'empty graph' do
       it 'returns empty matching' do
-        skip('Coming soon, to a graph near you!')
         g = graph_class.new
         m = described_class.new(g).match
         expect(m).to be_empty
+      end
+    end
+
+    context 'single vertex' do
+      it 'returns empty matching' do
+        g = graph_class.new
+        g.add_vertex(1)
+        m = described_class.new(g).match
+        expect(m).to be_empty
+      end
+    end
+
+    context 'two vertexes' do
+      it 'returns matching of size 1' do
+        g = graph_class[[1, 2, 1]]
+        m = described_class.new(g).match
+        expect(m.vertexes).to match_array([1, 2])
+        expect(m.weight(g)).to eq(1)
+      end
+    end
+
+    context 'three vertexes' do
+      it 'matches the edge with greater weight' do
+        g = graph_class[
+          [1, 2, 1],
+          [2, 3, 2],
+          [3, 1, 3]
+        ]
+        m = described_class.new(g).match
+        expect(m.vertexes).to match_array([3, 1])
+        expect(m.weight(g)).to eq(3)
       end
     end
   end
