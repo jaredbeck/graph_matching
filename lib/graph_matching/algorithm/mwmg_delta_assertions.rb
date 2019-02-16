80a84c83190717c4d183ebf519caa957714a7580
Jared Beck <jared@jaredbeck.com>
2019-02-16 00:38:13 -0500

    Lint: Style/MultipleComparison

--- lib/graph_matching/algorithm/mwmg_delta_assertions.rb
+++ lib/graph_matching/algorithm/mwmg_delta_assertions.rb
@@ -69,7 +69,7 @@ module GraphMatching
           end
           next if @best_edge[b].nil?
           i, j = @edges[@best_edge[b]].to_a
-          unless @in_blossom[i] == b || @in_blossom[j] == b
+          unless @in_blossom.values_at(i, j).include?(b)
             raise 'Assertion failed'
           end
           unless @in_blossom[i] != b || @in_blossom[j] != b
